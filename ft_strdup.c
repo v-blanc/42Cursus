04847b88dfc486822cf1fa09006a365c97db16fd
v-blanc <valentin.blanc.code@gmail.com>
2024-10-10 17:23:07 +0200

    add ft_strdup.c

---
+++ ft_strdup.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vblanc <vblanc@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 17:17:09 by vblanc            #+#    #+#             */
+/*   Updated: 2024/10/10 17:22:48 by vblanc           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*d;
+	size_t	len;
+
+	len = ft_strlen(s) + 1;
+	d = (char *)malloc(sizeof(char) * len);
+	if (!d)
+		return (NULL);
+	ft_memcpy(d, s, len);
+	return (d);
+}
