6b91481366dd20cf92f2cda3c1235dc018eaf7cf
v-blanc <valentin.blanc.code@gmail.com>
2024-10-10 02:16:42 +0200

    add ft_memmove

---
+++ ft_memmove.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vblanc <temp@temp.com>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 01:40:04 by vblanc            #+#    #+#             */
+/*   Updated: 2024/10/10 02:15:01 by vblanc           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+#include <string.h>
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char		*d;
+	const unsigned char	*s;
+
+	d = dest;
+	s = src;
+	if (s < d)
+	{
+		d += n - 1;
+		s += n - 1;
+		while (n-- > 0)
+			*d-- = *s--;
+	}
+	else
+		ft_memcpy(d, s, n);
+	return (dest);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vblanc <temp@temp.com>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 20:58:15 by vblanc            #+#    #+#             */
-/*   Updated: 2024/10/10 01:38:45 by vblanc           ###   ########.fr       */
+/*   Updated: 2024/10/10 02:07:24 by vblanc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,10 @@
 
 /* Libc functions */
 void	*ft_memset(void *s, int c, size_t n);
-void	bzero(void *s, size_t n);
-void	*memcpy(void *dest, const void *src, size_t n);
-void	*memccpy(void *dest, const void *src, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 
