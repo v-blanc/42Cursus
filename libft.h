ec5f295f8e5cf5c2e0690772cd494c2b873b7394
v-blanc <valentin.blanc.code@gmail.com>
2024-10-10 15:55:42 +0200

    add ft_isdigit

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vblanc <vblanc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 20:58:15 by vblanc            #+#    #+#             */
-/*   Updated: 2024/10/10 15:44:30 by vblanc           ###   ########.fr       */
+/*   Updated: 2024/10/10 15:55:22 by vblanc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,6 @@ char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_isalpha(int c);
+int		ft_isdigit(int c);
 
 #endif
