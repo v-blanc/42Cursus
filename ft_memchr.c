e201f849f370b9c24487e6a28540b8a673472e5b
v-blanc <valentin.blanc.code@gmail.com>
2024-10-10 02:19:49 +0200

    update 42 headers

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,25 +1,11 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vblanc <vblanc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/09 22:56:13 by vblanc            #+#    #+#             */
-/*   Updated: 2024/10/10 02:19:07 by vblanc           ###   ########.fr       */
+/*   Created: 2024/10/10 02:17:46 by vblanc            #+#    #+#             */
+/*   Updated: 2024/10/10 02:19:04 by vblanc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "libft.h"
-
-void	*ft_memcpy(void *dest, const void *src, size_t n)
-{
-	unsigned char		*d;
-	const unsigned char	*s;
-
-	d = (unsigned char *)dest;
-	s = (const unsigned char *)src;
-	while (n--)
-		*d++ = *s++;
-	return (dest);
-}
