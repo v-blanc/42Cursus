e98466f96035873269d50d59eee8dd4619469276
v-blanc <valentin.blanc.code@gmail.com>
2024-11-18 15:37:15 +0100

    opti substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: vblanc <vblanc@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 23:45:07 by vblanc            #+#    #+#             */
-/*   Updated: 2024/11/08 13:19:43 by vblanc           ###   ########.fr       */
+/*   Updated: 2024/11/18 15:31:42 by vblanc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,12 @@ The substring begins at index ’start’ and is of maximum size ’len’. */
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
+	size_t	len_s;
 
-	if (ft_strlen(s) - start < len)
-		len = ft_strlen(s) - start;
-	if (start > ft_strlen(s))
+	len_s = ft_strlen(s);
+	if (len_s - start < len)
+		len = len_s - start;
+	if (start > len_s)
 		return (ft_strdup(""));
 	sub = (char *)malloc(sizeof(char) * len + 1);
 	if (!sub)
