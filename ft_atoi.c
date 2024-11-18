96820709825c20edf804f56e59d311b1968a3956
v-blanc <valentin.blanc.code@gmail.com>
2024-11-18 15:25:21 +0100

    fix atoi for LONG_MAX and LONG_MIN and fix split

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,11 +6,22 @@
 /*   By: vblanc <vblanc@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 16:33:31 by vblanc            #+#    #+#             */
-/*   Updated: 2024/11/04 14:15:19 by vblanc           ###   ########.fr       */
+/*   Updated: 2024/11/18 15:00:15 by vblanc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static void	ft_sub_atoi(const char *str, int *i, int *sign)
+{
+	while (ft_isspace(str[(*i)]))
+		(*i)++;
+	if (str[(*i)] == '-' || str[(*i)] == '+')
+	{
+		if (str[(*i)++] == '-')
+			(*sign) = -1;
+	}
+}
 
 /* Converts the initial portion of the string pointed to by ’str’ to int. */
 int	ft_atoi(const char *str)
@@ -18,18 +29,23 @@ int	ft_atoi(const char *str)
 	int		i;
 	int		sign;
 	long	res;
+	long	prev;
 
 	i = 0;
 	sign = 1;
 	res = 0;
-	while (ft_isspace(str[i]))
-		i++;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i++] == '-')
-			sign = -1;
-	}
+	ft_sub_atoi(str, &i, &sign);
 	while (ft_isdigit(str[i]))
+	{
+		prev = res;
 		res = res * 10 + (str[i++] - '0');
+		if (res < prev)
+		{
+			if (sign == 1)
+				return (-1);
+			else
+				return (0);
+		}
+	}
 	return ((int)res * sign);
 }
--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: vblanc <vblanc@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 12:41:54 by vblanc            #+#    #+#             */
-/*   Updated: 2024/11/18 12:06:21 by vblanc           ###   ########.fr       */
+/*   Updated: 2024/11/18 15:23:49 by vblanc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,31 +31,31 @@ static int	ft_count_words(char const *s, char c)
 	return (count);
 }
 
-static int	ft_sub_split(char const *s, char c, char **tab)
+static int	ft_sub_split(char const *s, char c, char **tab, int *i)
 {
 	char	*new_s;
-	int		i;
 
-	i = 0;
 	while (*s)
 	{
 		new_s = (char *)s;
 		while (*new_s && *new_s != c)
 			new_s++;
 		if (new_s - s > 0)
-			tab[i++] = ft_substr(s, 0, new_s - s);
-		if (tab[i - 1] == NULL)
 		{
-			while (i > 0)
-				free(tab[--i]);
-			free(tab);
-			return (0);
+			tab[(*i)++] = ft_substr(s, 0, new_s - s);
+			if (tab[(*i) - 1] == NULL)
+			{
+				while ((*i) > 0)
+					free(tab[--(*i)]);
+				free(tab);
+				return (0);
+			}
 		}
 		if (!*new_s)
 			break ;
 		s = new_s + 1;
 	}
-	tab[i] = NULL;
+	tab[(*i)] = NULL;
 	return (1);
 }
 
@@ -65,13 +65,15 @@ The array is NULL-terminated. */
 char	**ft_split(char const *s, char c)
 {
 	char	**tab;
+	int		i;
 
 	if (!s)
 		return (NULL);
 	tab = (char **)malloc(sizeof(char *) * (ft_count_words(s, c) + 1));
 	if (!tab)
 		return (NULL);
-	if (!ft_sub_split(s, c, tab))
+	i = 0;
+	if (!ft_sub_split(s, c, tab, &i))
 		return (NULL);
 	return (tab);
 }
