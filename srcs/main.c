09648edcc3b2a3902e53ed0c478f89700c7ebcbc
v-blanc <valentin.blanc.code@gmail.com>
2025-03-14 22:08:03 +0100

    update main.c

--- srcs/main.c
+++ srcs/main.c
@@ -1,29 +1,44 @@
 #include "../include/minishell.h"
 
+char	*set_readline_prompt(void)
+{
+	char	*readline_prompt;
+	char	*pwd;
+
+	readline_prompt = "minishell:";
+	pwd = getcwd(NULL, 0);
+	if (pwd)
+		readline_prompt = ft_strjoin(readline_prompt, pwd);
+	readline_prompt = ft_strjoin(readline_prompt, "$ ");
+	return (readline_prompt);
+}
+
 void	set_input(void)
 {
 	char	*input;
+	char	*readline_prompt;
 
 	rl_clear_history();
 	while (1)
 	{
-		printf("minishell:%s$ ", getenv("PWD"));
-		input = readline("");
+		readline_prompt = set_readline_prompt();
+		input = readline(readline_prompt);
+		free(readline_prompt);
 		if (!input)
 		{
 			printf("error\n");
 			return ;
 		}
-        // TODO: remove when exit is implemented
+		// TODO: remove when exit is implemented
 		if (ft_strcmp(input, "exit") == 0)
 			exit(0);
-		if (ft_strcmp(input, "") == 0)
-			printf("\n");
-		// printf("%s\n", input);
 		add_history(input);
+		cd(input); // TESTING CD
 		free(input);
 	}
 }
+
+extern char	**environ;
 
 int	main(void)
 {
