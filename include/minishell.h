4f716eea103eb8b4aaf5841e6c98a0c3efe93aa6
v-blanc <valentin.blanc.code@gmail.com>
2025-04-03 20:06:26 +0200

    update minishell.h libs

--- include/minishell.h
+++ include/minishell.h
@@ -4,6 +4,7 @@
 /* Libraries */
 
 # include "../libft/libft.h"
+# include <errno.h>
 # include <fcntl.h>
 # include <readline/history.h>
 # include <readline/readline.h>
@@ -11,6 +12,7 @@
 # include <stdbool.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <string.h>
 # include <unistd.h>
 
 /* Variables */
