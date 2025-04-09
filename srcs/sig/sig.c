7e1abe007eaae1334adfb309483d1bd910716d1c
v-blanc <valentin.blanc.code@gmail.com>
2025-04-09 17:42:43 +0200

    update sig.c

--- srcs/sig/sig.c
+++ srcs/sig/sig.c
@@ -4,17 +4,16 @@ void	sig_handler(int sig)
 {
 	if (sig == SIGINT)
 	{
-		printf("^C\n");
-		// TODO: cleanup everything
-		printf("temp: ctrl+c have been pressed\n");
-		exit(130);
+		printf("\n");
+		rl_on_new_line();
+		rl_redisplay();
 	}
-	else if (sig == SIGQUIT)
-	{
-		printf("^\\");
-		// TODO: cleanup everything
-		printf("temp: ctrl+\\ have been pressed\n");
-	}
+	// else if (sig == SIGQUIT)
+	// {
+	// 	printf("^\\");
+	// 	//TODO: cleanup everything
+	// 	printf("temp: ctrl+\\ have been pressed\n");
+	// }
 }
 
 void	init_sig(void)
