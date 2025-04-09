cfd2bbb2ac581cf43b42c0fb94e4a81e2bfdf6ad
v-blanc <valentin.blanc.code@gmail.com>
2025-04-09 16:08:07 +0200

    update get_env_value.c now working and norminette

--- srcs/parsing/get_env_value.c
+++ srcs/parsing/get_env_value.c
@@ -1,59 +1,72 @@
 #include "../../include/minishell.h"
 
-int	get_env_value(char **input_with_env, char *input_str,
+int	found_value(char **input_with_env, char *input_str, int *ind,
 		t_garbage_collector **head)
 {
 	char	*value;
 	char	*name;
 	char	*var_env;
-	int		i;
-	int		j;
-	int		a;
 
+	value = NULL;
+	if (!ft_strncmp(&input_str[ind[0]], "$$", 2))
+		value = ft_itoa(getpid());
+	else if (!ft_strncmp(&input_str[ind[0]], "$?", 2))
+		value = "9999999"; // TODO: get exit status
+	else
+	{
+		(void)head;
+		name = ft_substr(input_str, ind[0] + 1, ind[1] - ind[0] - 1);
+		if (!name)
+			return (1);
+		var_env = getenv(name);
+		if (var_env)
+			value = var_env;
+		else
+			value = "";
+	}
+	ft_strlcat((*input_with_env), value, ft_strlen((*input_with_env))
+		+ ft_strlen(value) + 1);
+	return (0);
+}
+
+void	detect_var(char **input_with_env, char *input_str, int *ind, int *i)
+{
+	while (input_str[ind[0]] && ft_strncmp(&input_str[ind[0]], "$", 1))
+		(*input_with_env)[(*i)++] = input_str[ind[0]++];
+	if (!input_str[ind[0]])
+		return ;
+	ind[1] = ind[0] + 1;
+	while (input_str[ind[1]] && ft_isalnum(input_str[ind[1]]))
+		ind[1]++;
+	if (ind[1] == ind[0] + 1 && input_str[ind[0]] == '$'
+		&& input_str[ind[1]] != '$' && input_str[ind[1]] != '?')
+		(*input_with_env)[(*i)++] = '$';
+}
+
+int	get_env_value(char **input_with_env, char *input_str,
+		t_garbage_collector **head)
+{
+	int	i;
+	int	ind[2];
+
+	// TODO: fix the size `10000`?
 	(*input_with_env) = gc_malloc(sizeof(char) * 10000, head);
 	if (!(*input_with_env))
 		return (1);
-	a = 0;
 	i = 0;
-	while (input_str[a])
+	ind[0] = 0;
+	while (input_str[ind[0]])
 	{
-		value = NULL;
-		printf("input_with_env: '%s'\n", (*input_with_env));
-		while (input_str[a] && ft_strncmp(&input_str[a], "$", 1))
-			(*input_with_env)[i++] = input_str[a++];
-		if (!input_str[a])
+		detect_var(input_with_env, input_str, ind, &i);
+		if (!input_str[ind[0]])
 			return (0);
-		j = a + 1;
-		while (input_str[j] && ft_isalnum(input_str[j]))
-			j++;
-		if (!ft_strncmp(&input_str[a], "$$", 2))
-			value = ft_itoa(getpid());
-		else if (!ft_strncmp(&input_str[a], "$?", 2))
-			value = "100000"; // TODO: get exit status
-		else
-		{
-			name = &input_str[a + 1];
-			name[j - a - 1] = '\0';
-			var_env = getenv(name);
-			if (var_env)
-				value = var_env;
-			else
-				value = "";
-		}
-		printf("input_with_env: '%s'\n", (*input_with_env));
-		printf("value: '%s'\n", value);
-		// Add 'value' at the end of input_with_env
-		ft_strlcat((*input_with_env), value, ft_strlen((*input_with_env))
-			+ ft_strlen(value) + 1);
-		printf("input_with_env: '%s'\n", (*input_with_env));
-		printf("a= %d, i= %d, j= %d\n", a, i, j);
-		a = j;
-		i = ft_strlen((*input_with_env)) + 1;
-		if (value[0] == '\0')
-		{
-			a++;
-			i++;
-		}
+		if (found_value(input_with_env, input_str, ind, head))
+			return (1);
+		if (!ft_strncmp(&input_str[ind[0]], "$$", 2)
+			|| !ft_strncmp(&input_str[ind[0]], "$?", 2))
+			ind[1]++;
+		ind[0] = ind[1];
+		i = ft_strlen((*input_with_env));
 	}
 	return (0);
 }
@@ -65,6 +78,6 @@ int	testing_parsing(char *input_str, t_garbage_collector **head)
 	input_with_env = NULL;
 	if (get_env_value(&input_with_env, input_str, head))
 		return (1);
-	printf("\n\n>>>> %s\n", input_with_env);
+	printf("\n>>>> %s\n\n", input_with_env);
 	return (0);
 }
