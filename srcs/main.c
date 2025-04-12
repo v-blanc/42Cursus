1abcac25e8ef2b0cdcca0f86e92cd2b68deb3c82
v-blanc <valentin.blanc.code@gmail.com>
2025-04-13 01:09:48 +0200

    remove useless parts

--- srcs/main.c
+++ srcs/main.c
@@ -24,61 +24,61 @@ char	*set_readline_prompt(t_gc **head)
 	return (final_rl_prompt);
 }
 
-int	testing(char *input, t_gc **head)
-{
-	char	**split_input;
-	char	*new_input;
+// int	testing(char *input, t_gc **head)
+// {
+// 	char	**split_input;
+// 	char	*new_input;
 
-	if (!input)
-		return (1);
-	if (!input[0])
-	{
-		// TODO: set exit status to 130
-		return (1);
-	}
-	if (test_quotes_validity(input))
-		return (1);
-	new_input = NULL;
-	parse_quotes(input, &new_input, head);
-	split_input = ft_split(new_input, ' ');
-	if (!split_input)
-		return (1);
-	if (!ft_strcmp(split_input[0], "exit"))
-		return (1);
-	else if (!ft_strcmp(split_input[0], "cd"))
-		cd(split_input[1], head);
-	else if (!ft_strcmp(split_input[0], "echo"))
-	{
-		printf("Caution: echo have to be reworked\n");
-		echo(split_input[1], false);
-	}
-	else if (!ft_strcmp(split_input[0], "env"))
-		env();
-	else if (ft_strcmp(input, "exit") == 0)
-	{
-		free(input);
-		gc_free_all(*head);
-		rl_clear_history();
-		exit(0);
-	}
-	else if (!ft_strcmp(split_input[0], "export"))
-	{
-		if (!split_input[2])
-			export(split_input[1], "", head);
-		else
-			export(split_input[1], split_input[2], head);
-	}
-	else if (!ft_strcmp(split_input[0], "pwd"))
-		pwd();
-	else if (!ft_strcmp(split_input[0], "unset"))
-		unset(split_input + 1, head);
-	else
-		printf("minishell: Command not found\n");
-	for (int i = 0; split_input[i]; i++)
-		free(split_input[i]);
-	free(split_input);
-	return (0);
-}
+// 	if (!input)
+// 		return (1);
+// 	if (!input[0])
+// 	{
+// 		// TODO: set exit status to 130
+// 		return (1);
+// 	}
+// 	if (test_quotes_validity(input))
+// 		return (1);
+// 	new_input = NULL;
+// 	parse_quotes(input, &new_input, head);
+// 	split_input = ft_split(new_input, ' ');
+// 	if (!split_input)
+// 		return (1);
+// 	if (!ft_strcmp(split_input[0], "exit"))
+// 		return (1);
+// 	else if (!ft_strcmp(split_input[0], "cd"))
+// 		cd(split_input[1], head);
+// 	else if (!ft_strcmp(split_input[0], "echo"))
+// 	{
+// 		printf("Caution: echo have to be reworked\n");
+// 		echo(split_input[1], false);
+// 	}
+// 	else if (!ft_strcmp(split_input[0], "env"))
+// 		env();
+// 	else if (ft_strcmp(input, "exit") == 0)
+// 	{
+// 		free(input);
+// 		gc_free_all(*head);
+// 		rl_clear_history();
+// 		exit(0);
+// 	}
+// 	else if (!ft_strcmp(split_input[0], "export"))
+// 	{
+// 		if (!split_input[2])
+// 			export(split_input[1], "", head);
+// 		else
+// 			export(split_input[1], split_input[2], head);
+// 	}
+// 	else if (!ft_strcmp(split_input[0], "pwd"))
+// 		pwd();
+// 	else if (!ft_strcmp(split_input[0], "unset"))
+// 		unset(split_input + 1, head);
+// 	else
+// 		printf("minishell: Command not found\n");
+// 	for (int i = 0; split_input[i]; i++)
+// 		free(split_input[i]);
+// 	free(split_input);
+// 	return (0);
+// }
 
 void	set_input(t_context *context, t_gc **head)
 {
