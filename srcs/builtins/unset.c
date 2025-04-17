a03c18e1fdb70c86028bb07a0a9e0c57c0653387
v-blanc <valentin.blanc.code@gmail.com>
2025-04-17 19:39:37 +0200

    fix multi unset

--- srcs/builtins/unset.c
+++ srcs/builtins/unset.c
@@ -18,28 +18,38 @@ static int	to_unset_found(char **to_unset)
 	return (found);
 }
 
-static int	unset_env(char ***new_env, char *name, t_gc **head)
+static int	sub_unset_env(char **to_unset, int *k, char **name, t_gc **head)
 {
-	int	i;
-	int	j;
-	int	len_name;
+	*name = gc_strjoin(to_unset[(*k)++], "=", head);
+	if (!*name)
+		return (1);
+	return (0);
+}
+static int	unset_env(char ***new_env, char **to_unset, t_gc **head)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	*name;
 
 	i = 0;
 	j = 0;
-	name = gc_strjoin(name, "=", head);
-	if (!name)
-		return (1);
-	len_name = ft_strlen(name);
 	while (environ[i])
 	{
-		if (!ft_strncmp(environ[i], name, len_name))
-			i++;
-		else
+		k = 0;
+		while (environ[i] && to_unset[k])
 		{
-			(*new_env)[j] = environ[i];
-			i++;
-			j++;
+			if (sub_unset_env(to_unset, &k, &name, head))
+				return (1);
+			if (!ft_strncmp(environ[i], name, ft_strlen(name)))
+			{
+				i++;
+				k = 0;
+				continue ;
+			}
 		}
+		if (environ[i])
+			(*new_env)[j++] = environ[i++];
 	}
 	return (0);
 }
@@ -48,23 +58,17 @@ int	unset(char **to_unset, t_gc **head)
 {
 	char	**new_env;
 	int		new_env_size;
-	int		i;
 
 	if (to_unset == NULL || to_unset[0] == NULL)
 		return (0);
 	if (to_unset_found(to_unset) == 0)
 		return (0);
 	new_env_size = ft_strlen_array(environ) - to_unset_found(to_unset);
-	new_env = (char **)gc_malloc_array_perm(new_env_size, head);
+	new_env = (char **)gc_malloc_array_perm(new_env_size + 1, head);
 	if (!new_env)
 		return (1);
-	i = 0;
-	while (to_unset[i])
-	{
-		if (unset_env(&new_env, to_unset[i], head))
-			return (1);
-		i++;
-	}
+	if (unset_env(&new_env, to_unset, head))
+		return (1);
 	environ = new_env;
 	return (0);
 }
