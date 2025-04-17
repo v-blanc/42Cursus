b41f2af2d56017ea667b495dd18a33fccfb7b0c7
v-blanc <valentin.blanc.code@gmail.com>
2025-04-17 05:23:25 +0200

    update init_table.c and add print_init_info() in main.c

--- srcs/init_table.c
+++ srcs/init_table.c
@@ -6,32 +6,47 @@
 /*   By: vblanc <vblanc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 17:17:33 by vblanc            #+#    #+#             */
-/*   Updated: 2025/04/17 05:06:33 by vblanc           ###   ########.fr       */
+/*   Updated: 2025/04/17 05:14:56 by vblanc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static int	init_philo(t_table *table)
+static int	init_philos(t_table *table)
 {
-	t_philo	*philo;
+	t_philo	*philos;
 	int		i;
 
-	philo = (t_philo *)malloc(sizeof(t_philo) * table->n_philo);
-	if (!philo)
+	philos = (t_philo *)malloc(sizeof(t_philo) * table->n_philo);
+	if (!philos)
 		return (1);
 	i = 0;
 	while (i < table->n_philo)
 	{
-		philo[i].id = i + 1;
-		philo[i].eat_count = 0;
-		philo[i].time_start = get_curr_time();
-		philo[i].time_last_eat = philo[i].time_start;
-		philo[i].l_fork = &table->forks[(i + 1) % table->n_philo];
-		philo[i].r_fork = &table->forks[i];
-		philo[i].table_lock = &table->table_lock;
-		philo[i].write_lock = &table->write_lock;
-		philo[i].table = table;
+		philos[i].id = i + 1;
+		philos[i].eat_count = 0;
+		philos[i].time_start = get_curr_time();
+		philos[i].time_last_eat = philos[i].time_start;
+		philos[i].l_fork = &table->forks[(i + 1) % table->n_philo];
+		philos[i].r_fork = &table->forks[i];
+		philos[i].table_lock = &table->table_lock;
+		philos[i].write_lock = &table->write_lock;
+		philos[i].table = table;
+		i++;
+	}
+	table->philos = philos;
+	return (0);
+}
+
+static int	init_forks(t_table *table)
+{
+	int	i;
+
+	i = 0;
+	while (i < table->n_philo)
+	{
+		if (pthread_mutex_init(&table->forks[i], NULL))
+			return (1);
 		i++;
 	}
 	return (0);
@@ -57,7 +72,9 @@ int	init_table(t_table **table, int argc, char **argv)
 	if (pthread_mutex_init(&(*table)->table_lock, NULL)
 		|| pthread_mutex_init(&(*table)->write_lock, NULL))
 		return (1);
-	if (init_philo(*table))
+	if (init_forks(*table))
+		return (1);
+	if (init_philos(*table))
 		return (1);
 	return (0);
 }
--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,37 @@
 /*   By: vblanc <vblanc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 14:55:08 by vblanc            #+#    #+#             */
-/*   Updated: 2025/04/17 05:01:48 by vblanc           ###   ########.fr       */
+/*   Updated: 2025/04/17 05:22:19 by vblanc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+static void	print_init_info(t_table *table)
+{
+	printf("Table: %p\n", table);
+	printf("Number of philosophers: %d\n", table->n_philo);
+	printf("Time to die: %zu\n", table->time_to_die);
+	printf("Time to eat: %zu\n", table->time_to_eat);
+	printf("Time to sleep: %zu\n", table->time_to_sleep);
+	printf("Must eat: %d\n", table->must_eat);
+	printf("Table lock: %p\n", &table->table_lock);
+	printf("Write lock: %p\n", &table->write_lock);
+	printf("\n");
+	for (int i = 0; i < table->n_philo; i++)
+	{
+		printf("Philo id: %d\n", table->philos[i].id);
+		printf("Eat count: %d\n", table->philos[i].eat_count);
+		printf("Time start: %zu\n", table->philos[i].time_start);
+		printf("Time last eat: %zu\n", table->philos[i].time_last_eat);
+		printf("Left fork: %p\n", table->philos[i].l_fork);
+		printf("Right fork: %p\n", table->philos[i].r_fork);
+		printf("Table lock: %p\n", table->philos[i].table_lock);
+		printf("Write lock: %p\n", table->philos[i].write_lock);
+		printf("Table: %p\n", table->philos[i].table);
+		printf("\n");
+	}
+}
 
 int	main(int argc, char **argv)
 {
@@ -18,21 +44,8 @@ int	main(int argc, char **argv)
 
 	if (!is_valid_input(argc, argv))
 		return (1);
-	printf("Current time is: %zu\n", get_curr_time());
 	if (init_table(&table, argc, argv))
 		return (1);
-	// for (int i = 0; i < ft_atoi(argv[1]); i++)
-	// {
-	// 	printf("Philo id: %d\n", table->philos[i].id);
-	// 	printf("Time to die: %zu\n", table->philos[i].time_to_die);
-	// 	printf("Time to eat: %zu\n", table->philos[i].time_to_eat);
-	// 	printf("Time to sleep: %zu\n", table->philos[i].time_to_sleep);
-	// 	printf("Must eat: %d\n", table->philos[i].must_eat);
-	// 	printf("\n");
-	// }
-	// for (int i = 0; i < ft_atoi(argv[1]); i++)
-	// {
-	// 	pthread_join(table->philos[i].thread, NULL);
-	// }
+	// print_init_info(table);
 	return (0);
 }
