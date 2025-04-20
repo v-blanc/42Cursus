0e790b706c452a819efcb16fd77dba652198e06d
v-blanc <valentin.blanc.code@gmail.com>
2025-04-20 02:56:09 +0200

    update sig.c for ctrl-\

--- srcs/sig/sig.c
+++ srcs/sig/sig.c
@@ -2,13 +2,12 @@
 
 void	sig_handler(int sig)
 {
-	if (sig == SIGINT)
-	{
-		printf("\n");
-		rl_on_new_line();
-		rl_replace_line("", 0);
-		rl_redisplay();
-	}
+	(void)sig;
+	// printf("\n");
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
 }
 
 void	init_sig(void)
@@ -20,5 +19,5 @@ void	init_sig(void)
 	sa.sa_handler = sig_handler;
 	sigemptyset(&sa.sa_mask);
 	sigaction(SIGINT, &sa, NULL);
-	sigaction(SIGQUIT, &sa, NULL);
+	signal(SIGQUIT, SIG_IGN);
 }
