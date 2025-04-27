ff8bdcc3496b374feb8fff75b43dd5482cf278b4
v-blanc <valentin.blanc.code@gmail.com>
2025-04-27 19:16:13 +0200

    fix env to print only vars with value

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yabokhar <yabokhar@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: vblanc <vblanc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 18:33:43 by yabokhar          #+#    #+#             */
-/*   Updated: 2025/04/18 18:34:48 by yabokhar         ###   ########.fr       */
+/*   Updated: 2025/04/27 19:12:02 by vblanc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,29 @@
 
 extern char	**environ;
 
-/*static int	print_error(char *argument, int error, int status)
-
-{
-	print(2, "env: ‘%s’: %s", argument, strerror(error));
-	return (status);
-}*/
-
 int	env(t_ast *ast, t_context **context)
 {
-	//struct stat	buffer;
-	size_t		i;
-	//char 		**arguments = ast->u_data.s_cmd.args;
+	size_t	i;
+	int		j;
+
 	i = 0;
-	while (*ast->u_data.s_cmd.args && ft_strcmp(*ast->u_data.s_cmd.args, "env") == 0)
+	while (*ast->u_data.s_cmd.args && ft_strcmp(*ast->u_data.s_cmd.args,
+			"env") == 0)
 		ast->u_data.s_cmd.args++;
 	if (*ast->u_data.s_cmd.args)
 		return (execute_ast(ast, *context));
 	else
 	{
-		i = -1;
-		while (environ[++i])
-			print(1, "%s\n", environ[i]);
+		i = 0;
+		while (environ[i])
+		{
+			j = 0;
+			while (environ[i][j] && environ[i][j] != '=')
+				j++;
+			if (environ[i][j] == '=')
+				print(1, "%s\n", environ[i]);
+			i++;
+		}
 		return (EXIT_SUCCESS);
 	}
 }
