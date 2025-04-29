00a9ded4d0649eba9b9df9c829ddeab68fe1af4e
v-blanc <valentin.blanc.code@gmail.com>
2025-04-29 20:45:36 +0200

    add ex05

---
+++ ex05/srcs/Harl.cpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vblanc <vblanc@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/29 20:13:02 by vblanc            #+#    #+#             */
+/*   Updated: 2025/04/29 20:45:22 by vblanc           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl(void)
+{
+}
+
+Harl::~Harl(void)
+{
+}
+
+void Harl::debug(void) const
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info(void) const
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger!";
+	std::cout << "If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void Harl::warning(void) const
+{
+	std::cout << "I think I deserve to have some extra bacon for free.";
+	std::cout << "I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void Harl::error(void) const
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void Harl::complain(std::string level) const
+{
+	void (Harl::*f[4])(void) const = {&Harl::debug, &Harl::info, &Harl::warning,
+		&Harl::error};
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	for (int i = 0; i < 4; i++)
+	{
+		if (level == levels[i])
+		{
+			(this->*f[i])();
+			return ;
+		}
+	}
+	std::cout << "[Harl is complaining about something,";
+	std::cout << "but I don't know what...]" << std::endl;
+}
