6e58577b7c32bfd80de3ba436ac6461a79b996df
v-blanc <valentin.blanc.code@gmail.com>
2025-04-29 20:59:12 +0200

    update ex05

--- ex05/srcs/Harl.cpp
+++ ex05/srcs/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: vblanc <vblanc@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 20:13:02 by vblanc            #+#    #+#             */
-/*   Updated: 2025/04/29 20:48:59 by vblanc           ###   ########.fr       */
+/*   Updated: 2025/04/29 20:58:30 by vblanc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,20 +23,21 @@ Harl::~Harl(void)
 void Harl::debug(void) const
 {
 	std::cout << "[ DEBUG ]" << std::endl;
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger." << std::endl;
+	std::cout << "I really do!" << std::endl;
 }
 
 void Harl::info(void) const
 {
 	std::cout << "[ INFO ]" << std::endl;
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger!";
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger!" << std::endl;
 	std::cout << "If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void Harl::warning(void) const
 {
 	std::cout << "[ WARNING ]" << std::endl;
-	std::cout << "I think I deserve to have some extra bacon for free.";
+	std::cout << "I think I deserve to have some extra bacon for free." << std::endl;
 	std::cout << "I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
