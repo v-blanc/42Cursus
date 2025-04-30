a6260e09d14dd325c571f525e7cdb81799cb62a7
v-blanc <valentin.blanc.code@gmail.com>
2025-04-30 19:40:33 +0200

    add ex01

---
+++ ex01/srcs/main.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vblanc <vblanc@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/30 19:11:20 by vblanc            #+#    #+#             */
+/*   Updated: 2025/04/30 19:36:32 by vblanc           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+
+int	main(void)
+{
+	ClapTrap bob("Bob");
+	ScavTrap jim("Jim");
+	std::cout << std::endl;
+	std::cout << "[ " << bob.getName() << " has " << bob.getAttackDamage() << " attack damage ]" << std::endl;
+	std::cout << "[ " << bob.getName() << " has " << bob.getHitPoints() << " hit points ]" << std::endl;
+	std::cout << "[ " << bob.getName() << " has " << bob.getEnergyPoints() << " energy points ]" << std::endl;
+	std::cout << std::endl;
+	std::cout << "[ " << jim.getName() << " has " << jim.getAttackDamage() << " attack damage ]" << std::endl;
+	std::cout << "[ " << jim.getName() << " has " << jim.getHitPoints() << " hit points ]" << std::endl;
+	std::cout << "[ " << jim.getName() << " has " << jim.getEnergyPoints() << " energy points ]" << std::endl;
+	std::cout << std::endl;
+	{
+		bob.attack("John");
+		std::cout << "[ " << bob.getName() << " has " << bob.getEnergyPoints() << " energy points ]" << std::endl;
+		std::cout << std::endl;
+		jim.attack("John");
+		std::cout << "[ " << jim.getName() << " has " << jim.getEnergyPoints() << " energy points ]" << std::endl;
+		std::cout << std::endl;
+		jim.takeDamage(20);
+		std::cout << "[ " << jim.getName() << " has " << jim.getHitPoints() << " hit points ]" << std::endl;
+		std::cout << std::endl;
+		jim.beRepaired(10);
+		std::cout << "[ " << jim.getName() << " has " << jim.getHitPoints() << " hit points ]" << std::endl;
+		std::cout << std::endl;
+		jim.guardGate();
+	}
+	std::cout << std::endl;
+	return (0);
+}
