b2e340ac8ee550966b7fe3f71121306ea20b9383
v-blanc <valentin.blanc.code@gmail.com>
2025-05-06 12:38:45 +0200

    fix empty token issues

--- srcs/parsing/expander/merge_tokens.c
+++ srcs/parsing/expander/merge_tokens.c
@@ -6,38 +6,46 @@
 /*   By: vblanc <vblanc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 19:35:20 by vblanc            #+#    #+#             */
-/*   Updated: 2025/05/06 11:08:52 by vblanc           ###   ########.fr       */
+/*   Updated: 2025/05/06 12:36:21 by vblanc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	sub_merge_tokens(t_token *curr, t_token *next, t_gc **head)
+static int	sub_merge_tokens(t_token **curr, t_token *next, t_gc **head)
 {
 	char	*joined;
 	int		len_curr_value;
 	int		len_joined;
 
-	if (curr->type == WORD && next->type == WORD && curr->joined_next)
-	{
-		len_curr_value = ft_strlen(curr->value);
-		len_joined = len_curr_value + ft_strlen(next->value);
-		joined = gc_malloc(len_joined + 1, head);
-		if (!joined)
-			return (1);
-		if (curr->value[len_curr_value - 1] == '$' && next->quote != NO_QUOTE)
-			ft_strlcpy(joined, curr->value, len_curr_value);
-		else
-			ft_strlcpy(joined, curr->value, len_curr_value + 1);
-		ft_strlcat(joined, next->value, len_joined + 1);
-		gc_free(curr->value, head);
-		curr->value = joined;
-		curr->quote = next->quote;
-		curr->joined_next = next->joined_next;
-		curr->next = next->next;
-		gc_free(next, head);
-	}
+	len_curr_value = ft_strlen((*curr)->value);
+	len_joined = len_curr_value + ft_strlen(next->value);
+	joined = gc_malloc(len_joined + 1, head);
+	if (!joined)
+		return (1);
+	if ((*curr)->value[len_curr_value - 1] == '$' && next->quote != NO_QUOTE)
+		ft_strlcpy(joined, (*curr)->value, len_curr_value);
+	else
+		ft_strlcpy(joined, (*curr)->value, len_curr_value + 1);
+	ft_strlcat(joined, next->value, len_joined + 1);
+	(*curr)->value = joined;
+	(*curr)->quote = next->quote;
+	(*curr)->joined_next = next->joined_next;
+	(*curr)->next = next->next;
 	return (0);
+}
+
+static void	handle_empty_token(t_token **tokens, t_token *prev, t_token *curr,
+		t_token *next)
+{
+	if (curr->type == WORD && curr->quote == NO_QUOTE
+		&& ft_strlen(curr->value) == 0)
+	{
+		if (prev)
+			prev->next = next;
+		else
+			*tokens = next;
+	}
 }
 
 int	merge_tokens(t_token **tokens, t_gc **head)
@@ -51,20 +59,17 @@ int	merge_tokens(t_token **tokens, t_gc **head)
 	while (curr)
 	{
 		next = curr->next;
-		if (curr->type == WORD && curr->quote == NO_QUOTE
-			&& !ft_strlen(curr->value))
-		{
-			if (prev)
-				prev->next = next;
-			else
-				*tokens = next;
-		}
+		handle_empty_token(tokens, prev, curr, next);
+		prev = curr;
 		if (!next)
 			break ;
-		if (sub_merge_tokens(curr, next, head))
-			return (1);
-		prev = curr;
-		curr = next;
+		if (curr->type == WORD && next->type == WORD && curr->joined_next)
+		{
+			if (sub_merge_tokens(&curr, next, head))
+				return (1);
+		}
+		else
+			curr = curr->next;
 	}
 	return (0);
 }
