1cbd7ca8749fb042a2805b4a73f434ebfb1f657f
v-blanc <valentin.blanc.code@gmail.com>
2025-05-06 11:15:18 +0200

    fix empty  parsing

--- srcs/parsing/expander/merge_tokens.c
+++ srcs/parsing/expander/merge_tokens.c
@@ -6,7 +6,7 @@
 /*   By: vblanc <vblanc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 19:35:20 by vblanc            #+#    #+#             */
-/*   Updated: 2025/05/06 09:50:59 by vblanc           ###   ########.fr       */
+/*   Updated: 2025/05/06 11:08:52 by vblanc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,41 +18,53 @@ static int	sub_merge_tokens(t_token *curr, t_token *next, t_gc **head)
 	int		len_curr_value;
 	int		len_joined;
 
-	len_curr_value = ft_strlen(curr->value);
-	len_joined = len_curr_value + ft_strlen(next->value);
-	joined = gc_malloc(len_joined + 1, head);
-	if (!joined)
-		return (1);
-	if (curr->value[len_curr_value - 1] == '$' && next->quote != NO_QUOTE)
-		ft_strlcpy(joined, curr->value, len_curr_value);
-	else
-		ft_strlcpy(joined, curr->value, len_curr_value + 1);
-	ft_strlcat(joined, next->value, len_joined + 1);
-	gc_free(curr->value, head);
-	curr->value = joined;
-	curr->quote = next->quote;
-	curr->joined_next = next->joined_next;
-	curr->next = next->next;
-	gc_free(next, head);
+	if (curr->type == WORD && next->type == WORD && curr->joined_next)
+	{
+		len_curr_value = ft_strlen(curr->value);
+		len_joined = len_curr_value + ft_strlen(next->value);
+		joined = gc_malloc(len_joined + 1, head);
+		if (!joined)
+			return (1);
+		if (curr->value[len_curr_value - 1] == '$' && next->quote != NO_QUOTE)
+			ft_strlcpy(joined, curr->value, len_curr_value);
+		else
+			ft_strlcpy(joined, curr->value, len_curr_value + 1);
+		ft_strlcat(joined, next->value, len_joined + 1);
+		gc_free(curr->value, head);
+		curr->value = joined;
+		curr->quote = next->quote;
+		curr->joined_next = next->joined_next;
+		curr->next = next->next;
+		gc_free(next, head);
+	}
 	return (0);
 }
 
 int	merge_tokens(t_token **tokens, t_gc **head)
 {
+	t_token	*prev;
 	t_token	*curr;
 	t_token	*next;
 
+	prev = NULL;
 	curr = *tokens;
-	while (curr && curr->next)
+	while (curr)
 	{
 		next = curr->next;
-		if (curr->type == WORD && next->type == WORD && curr->joined_next)
+		if (curr->type == WORD && curr->quote == NO_QUOTE
+			&& !ft_strlen(curr->value))
 		{
-			if (sub_merge_tokens(curr, next, head))
-				return (1);
+			if (prev)
+				prev->next = next;
+			else
+				*tokens = next;
 		}
-		else
-			curr = curr->next;
+		if (!next)
+			break ;
+		if (sub_merge_tokens(curr, next, head))
+			return (1);
+		prev = curr;
+		curr = next;
 	}
 	return (0);
 }
