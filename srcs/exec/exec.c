37434fd2384dc3c83658ceb4a01e60a2283c2818
Yazid Bokhari <yabokhar@z3r10p6.42lyon.fr>
2025-05-18 17:22:27 +0200

    Refacto exec.c

--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -6,111 +6,91 @@
 /*   By: vblanc <vblanc@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 10:30:04 by yabokhar          #+#    #+#             */
-/*   Updated: 2025/05/18 14:22:25 by vblanc           ###   ########.fr       */
+/*   Updated: 2025/05/18 17:20:02 by yabokhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
 
-int			execute_ast(t_ast *node, t_context *ctx);
-int			handle_operators(t_ast *node, t_context *ctx);
+int			execute_command(t_ast *c, t_context *ctx);
+static bool	invalid_node_or_redirs(int *error, t_ast *node, t_context *ctx);
+static void	let_child_execute(t_ast *node, t_context *ctx, char *path);
+static int	wait_for_child(pid_t pid, t_context *ctx);
+static int	print_error_and_return(t_ast *node);
 
 extern char	**environ;
-
-int	execute_ast(t_ast *node, t_context *ctx)
-{
-	int	status;
-
-	status = 0;
-	if (!node)
-		return (0);
-	if (node->type == NODE_PAREN)
-	{
-		if (handle_redirections(node, ctx))
-			return (-1); // TODO: check if error is correctly handled
-		status = execute_ast(node->u_data.s_par.content, ctx);
-	}
-	else if (node->type == NODE_BINARY_OP)
-		status = handle_operators(node, ctx);
-	else if (node->type == NODE_PIPE)
-		status = handle_pipes(node, ctx);
-	else if (node->type == NODE_REDIR)
-		status = handle_redirections(node, ctx);
-	else if (node->type == NODE_CMD)
-		status = execute_command(node, ctx);
-	ctx->last_exit_status = status;
-	return (status);
-}
-
-int	handle_operators(t_ast *node, t_context *ctx)
-{
-	int	status;
-	int	type;
-
-	type = node->u_data.s_op.op;
-	status = execute_ast(node->u_data.s_op.left, ctx);
-	if (type == AND && status == 0)
-		return (execute_ast(node->u_data.s_op.right, ctx));
-	else if (type == OR && status != 0)
-		return (execute_ast(node->u_data.s_op.right, ctx));
-	return (type == AND);
-}
 
 int	execute_command(t_ast *c, t_context *ctx)
 {
 	char	*path;
+	int		error;
 	pid_t	pid;
-	int		status;
 
-	if (c->type == NODE_CMD && !c->u_data.s_cmd.args_count
-		&& !c->u_data.s_cmd.redir_count)
-		return (0);
-	if (handle_redirections(c, ctx))
-		return (1);
-	if (!c->u_data.s_cmd.args_count)
-		return (0);
-	if (c->u_data.s_cmd.args && c->u_data.s_cmd.args[0]
-		&& c->u_data.s_cmd.args[0][0] == '\0')
-	{
-		print(2, "minishell: '' command not found\n");
-		return (127);
-	}
+	if (invalid_node_or_redirs(&error, c, ctx))
+		return (error);
 	if (is_builtin(c->u_data.s_cmd.args[0]))
 		return (builtins_manager(c, &ctx));
 	path = track_paths(c->u_data.s_cmd.args[0], ctx->head);
-	if ((!is_builtin(c->u_data.s_cmd.args[0])) && (!path || access(path,
-				X_OK) != 0))
-	{
-		if (errno == ENOENT)
-		{
-			print(2, "minishell: %s: command not found\n",
-				c->u_data.s_cmd.args[0]);
-			return (127);
-		}
-		else
-		{
-			print(2, "minishell: %s: %s\n", c->u_data.s_cmd.args[0],
-				strerror(errno));
-			return (126);
-		}
-	}
+	if ((!is_builtin(c->u_data.s_cmd.args[0])) && (!path || access(path, X_OK)))
+		return (print_error_and_return(c));
 	if (ft_strcmp(c->u_data.s_cmd.args[0], "exit") == 0)
 		exit_(c->u_data.s_cmd.args_count, c->u_data.s_cmd.args + 1, &ctx);
 	pid = fork();
 	signal(SIGINT, SIG_IGN);
 	signal(SIGQUIT, SIG_IGN);
 	if (!pid)
+		let_child_execute(c, ctx, path);
+	return (wait_for_child(pid, ctx));
+}
+
+static bool	invalid_node_or_redirs(int *error, t_ast *node, t_context *ctx)
+{
+	const bool	command_node = (node->type == NODE_CMD);
+	const bool	no_args = (node->u_data.s_cmd.args_count == 0);
+	const bool	no_redirs = (node->u_data.s_cmd.redir_count == 0);
+	char		*command;
+	bool		empty_line;
+
+	*error = -1;
+	if (command_node && no_args && no_redirs)
+		*error = 0;
+	else if (handle_redirections(node, ctx))
+		*error = EXIT_FAILURE;
+	else if (no_args)
+		*error = 0;
+	if (no_args)
+		return (true);
+	command = node->u_data.s_cmd.args[0];
+	empty_line = (!command[0]);
+	if (command && empty_line)
 	{
-		signal(SIGINT, SIG_DFL);
-		signal(SIGQUIT, SIG_DFL);
-		execve(path, c->u_data.s_cmd.args, environ);
-		print(2, "minishell: %s: %s\n", c->u_data.s_cmd.args[0],
-			strerror(errno));
-		close(ctx->backup_fds[0]);
-		close(ctx->backup_fds[1]);
-		gc_free_all_perm(*ctx->head);
-		exit(126);
+		print(2, "minishell: '' command not found\n");
+		*error = 127;
 	}
+	if (*error == -1)
+		return (false);
+	return (true);
+}
+
+static void	let_child_execute(t_ast *node, t_context *ctx, char *path)
+{
+	const char	*command = node->u_data.s_cmd.args[0];
+	const char	*error_message = strerror(errno);
+
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+	execve(path, node->u_data.s_cmd.args, environ);
+	print(2, "minishell: %s: %s\n", command, error_message);
+	close(ctx->backup_fds[STDIN_FILENO]);
+	close(ctx->backup_fds[STDOUT_FILENO]);
+	gc_free_all_perm(*ctx->head);
+	exit(126);
+}
+
+static int	wait_for_child(pid_t pid, t_context *ctx)
+{
+	int	status;
+
 	status = 0;
 	waitpid(pid, &status, 0);
 	if (WIFSIGNALED(status))
@@ -119,4 +99,21 @@ int	execute_command(t_ast *c, t_context *ctx)
 		ctx->last_exit_status = WEXITSTATUS(status);
 	init_sig();
 	return (ctx->last_exit_status);
+}
+
+static int	print_error_and_return(t_ast *node)
+{
+	const char	*command = node->u_data.s_cmd.args[0];
+	const char	*error_message = strerror(errno);
+
+	if (errno == ENOENT)
+	{
+		print(2, "minishell: %s: command not found\n", command);
+		return (127);
+	}
+	else
+	{
+		print(2, "minishell: %s: %s\n", command, error_message);
+		return (126);
+	}
 }
