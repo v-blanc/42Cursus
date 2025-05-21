7687274c36ac4b55a92b8a6a9959cd1399114b05
v-blanc <valentin.blanc.code@gmail.com>
2025-05-21 12:08:20 +0200

    norm this file

--- srcs/parsing/wildcard/wildcard.c
+++ srcs/parsing/wildcard/wildcard.c
@@ -6,19 +6,18 @@
 /*   By: vblanc <vblanc@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 17:07:21 by vblanc            #+#    #+#             */
-/*   Updated: 2025/05/18 15:30:27 by vblanc           ###   ########.fr       */
+/*   Updated: 2025/05/21 12:06:29 by vblanc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 
-static int	wildcard_replace(t_token *tok, t_token **wildcard_tok, char **files)
+static int	wildcard_replace(t_token *tok, t_token **wildcard_tok, char **files,
+		t_gc **head)
 {
-	t_context	*ctx;
-	t_token		*tmp;
-	int			i;
+	t_token	*tmp;
+	int		i;
 
-	ctx = get_ptr();
 	(*wildcard_tok) = NULL;
 	tmp = NULL;
 	i = 0;
@@ -29,7 +28,7 @@ static int	wildcard_replace(t_token *tok, t_token **wildcard_tok, char **files)
 			i++;
 			continue ;
 		}
-		tmp = new_token(WORD, NO_QUOTE, files[i], ctx->head);
+		tmp = new_token(WORD, NO_QUOTE, files[i], head);
 		if (!tmp)
 			return (1);
 		token_add_back(wildcard_tok, tmp);
@@ -38,38 +37,48 @@ static int	wildcard_replace(t_token *tok, t_token **wildcard_tok, char **files)
 	return (0);
 }
 
-static int	sub_wildcard(t_token **tok, t_token **curr, t_token **prev,
-		char **files)
+static void	sub_sub_wildcard(t_token **curr, t_token *wildcard_tok)
 {
-	t_token	*wildcard_tok;
 	t_token	*next;
 
-	if ((*curr)->type == WORD && (*curr)->quote == NO_QUOTE)
+	next = (*curr)->next;
+	(*curr) = wildcard_tok;
+	while ((*curr)->next)
+		(*curr) = (*curr)->next;
+	(*curr)->next = next;
+}
+
+static int	sub_wildcard(t_token **tok, char **files, t_gc **head)
+{
+	t_token	*prev;
+	t_token	*curr;
+	t_token	*wildcard_tok;
+
+	prev = NULL;
+	curr = *tok;
+	while (curr)
 	{
-		if (ft_strchr((*curr)->value, '*'))
+		if (curr->type == WORD && curr->quote == NO_QUOTE
+			&& ft_strchr(curr->value, '*'))
 		{
-			if (wildcard_replace(*curr, &wildcard_tok, files))
+			if (wildcard_replace(curr, &wildcard_tok, files, head))
 				return (1);
 			if (!wildcard_tok)
 				return (0);
-			if ((*prev))
-				(*prev)->next = wildcard_tok;
+			if (prev)
+				prev->next = wildcard_tok;
 			else
 				*tok = wildcard_tok;
-			next = (*curr)->next;
-			(*curr) = wildcard_tok;
-			while ((*curr)->next)
-				(*curr) = (*curr)->next;
-			(*curr)->next = next;
+			sub_sub_wildcard(&curr, wildcard_tok);
 		}
+		prev = curr;
+		curr = curr->next;
 	}
 	return (0);
 }
 
 int	wildcard(t_token **tok, t_gc **head)
 {
-	t_token	*prev;
-	t_token	*curr;
 	char	**files;
 
 	files = NULL;
@@ -77,14 +86,7 @@ int	wildcard(t_token **tok, t_gc **head)
 		return (1);
 	if (!files)
 		return (0);
-	prev = NULL;
-	curr = *tok;
-	while (curr)
-	{
-		if (sub_wildcard(tok, &curr, &prev, files))
-			return (1);
-		prev = curr;
-		curr = curr->next;
-	}
+	if (sub_wildcard(tok, files, head))
+		return (1);
 	return (0);
 }
