c54c781de381a3c8684b45e902841c47991e53d2
v-blanc <valentin.blanc.code@gmail.com>
2025-06-09 21:22:43 +0200

    update main() and clear exercice

--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -3,44 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vblanc <vblanc@student.42lyon.fr>          +#+  +:+       +#+        */
+/*   By: vblanc <vblanc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 19:43:27 by vblanc            #+#    #+#             */
-/*   Updated: 2025/04/30 20:47:30 by vblanc           ###   ########.fr       */
+/*   Updated: 2025/06/09 21:15:04 by vblanc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 #include "FragTrap.hpp"
 
+void	get_stats(const ClapTrap &claptrap)
+{
+	std::cout << "[ " << claptrap.getName() << " have " << claptrap.getAttackDamage() << " attack damage ]" << std::endl;
+	std::cout << "[ " << claptrap.getName() << " have " << claptrap.getHitPoints() << " hit points ]" << std::endl;
+	std::cout << "[ " << claptrap.getName() << " have " << claptrap.getEnergyPoints() << " energy points ]" << std::endl;
+	std::cout << std::endl;
+}
+
 int	main(void)
 {
 	ClapTrap bob("Bob");
+	get_stats(bob);
 	FragTrap tom("Tom");
-	std::cout << std::endl;
-	std::cout << "[ " << bob.getName() << " have " << bob.getAttackDamage() << " attack damage ]" << std::endl;
-	std::cout << "[ " << bob.getName() << " have " << bob.getHitPoints() << " hit points ]" << std::endl;
-	std::cout << "[ " << bob.getName() << " have " << bob.getEnergyPoints() << " energy points ]" << std::endl;
-	std::cout << std::endl;
-	std::cout << "[ " << tom.getName() << " have " << tom.getAttackDamage() << " attack damage ]" << std::endl;
-	std::cout << "[ " << tom.getName() << " have " << tom.getHitPoints() << " hit points ]" << std::endl;
-	std::cout << "[ " << tom.getName() << " have " << tom.getEnergyPoints() << " energy points ]" << std::endl;
-	std::cout << std::endl;
-	{
-		bob.attack("John");
-		std::cout << "[ " << bob.getName() << " have " << bob.getEnergyPoints() << " energy points ]" << std::endl;
-		std::cout << std::endl;
-		tom.attack("John");
-		std::cout << "[ " << tom.getName() << " have " << tom.getEnergyPoints() << " energy points ]" << std::endl;
-		std::cout << std::endl;
-		tom.takeDamage(20);
-		std::cout << "[ " << tom.getName() << " have " << tom.getHitPoints() << " hit points ]" << std::endl;
-		std::cout << std::endl;
-		tom.beRepaired(10);
-		std::cout << "[ " << tom.getName() << " have " << tom.getHitPoints() << " hit points ]" << std::endl;
-		std::cout << std::endl;
-		tom.highFivesGuys();
-	}
+	get_stats(tom);
+	bob.attack("John");
+	get_stats(bob);
+	tom.attack("John");
+	get_stats(tom);
+	tom.takeDamage(20);
+	get_stats(tom);
+	tom.beRepaired(10);
+	get_stats(tom);
+	tom.highFivesGuys();
 	std::cout << std::endl;
 	return (0);
 }
