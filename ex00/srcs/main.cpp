cb01a53516afbc8c07c8f2945177f02dd2db8e6e
v-blanc <valentin.blanc.code@gmail.com>
2025-06-09 20:51:46 +0200

    update main()

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -3,49 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vblanc <vblanc@student.42lyon.fr>          +#+  +:+       +#+        */
+/*   By: vblanc <vblanc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 17:29:40 by vblanc            #+#    #+#             */
-/*   Updated: 2025/04/30 20:46:34 by vblanc           ###   ########.fr       */
+/*   Updated: 2025/06/09 20:50:36 by vblanc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
+void	get_stats(const ClapTrap &claptrap)
+{
+	std::cout << "[ " << claptrap.getName() << " have " << claptrap.getAttackDamage() << " attack damage ]" << std::endl;
+	std::cout << "[ " << claptrap.getName() << " have " << claptrap.getHitPoints() << " hit points ]" << std::endl;
+	std::cout << "[ " << claptrap.getName() << " have " << claptrap.getEnergyPoints() << " energy points ]" << std::endl;
+	std::cout << std::endl;
+}
+
 int	main(void)
 {
 	{
 		ClapTrap claptrap("Bob");
-		std::cout << std::endl;
-		std::cout << "[ " << claptrap.getName() << " have " << claptrap.getAttackDamage() << " attack damage ]" << std::endl;
-		std::cout << "[ " << claptrap.getName() << " have " << claptrap.getHitPoints() << " hit points ]" << std::endl;
-		std::cout << "[ " << claptrap.getName() << " have " << claptrap.getEnergyPoints() << " energy points ]" << std::endl;
-		std::cout << std::endl;
+		get_stats(claptrap);
 		claptrap.attack("NOTHING");
-		std::cout << "[ Now " << claptrap.getName() << " have " << claptrap.getEnergyPoints() << " energy points ]" << std::endl;
-		std::cout << std::endl;
+		get_stats(claptrap);
 		claptrap.takeDamage(2);
-		std::cout << "[ Now " << claptrap.getName() << " have " << claptrap.getHitPoints() << " hit points ]" << std::endl;
-		std::cout << "[ Now " << claptrap.getName() << " have " << claptrap.getEnergyPoints() << " energy points ]" << std::endl;
-		std::cout << std::endl;
+		get_stats(claptrap);
 		claptrap.beRepaired(4);
-		std::cout << "[ Now " << claptrap.getName() << " have " << claptrap.getHitPoints() << " hit points ]" << std::endl;
-		std::cout << "[ Now " << claptrap.getName() << " have " << claptrap.getEnergyPoints() << " energy points ]" << std::endl;
-		std::cout << std::endl;
+		get_stats(claptrap);
 		claptrap.takeDamage(12);
-		std::cout << "[ Now " << claptrap.getName() << " have " << claptrap.getHitPoints() << " hit points ]" << std::endl;
-		std::cout << std::endl;
+		get_stats(claptrap);
+		claptrap.beRepaired(2);
+		get_stats(claptrap);
 		claptrap.takeDamage(10000);
-		std::cout << "[ Now " << claptrap.getName() << " have " << claptrap.getHitPoints() << " hit points ]" << std::endl;
+		get_stats(claptrap);
 	}
 	std::cout << "-----------------------------------------------------------" << std::endl;
 	{
 		ClapTrap bob("Bob");
-		std::cout << std::endl;
-		std::cout << "[ " << bob.getName() << " have " << bob.getAttackDamage() << " attack damage ]" << std::endl;
-		std::cout << "[ " << bob.getName() << " have " << bob.getHitPoints() << " hit points ]" << std::endl;
-		std::cout << "[ " << bob.getName() << " have " << bob.getEnergyPoints() << " energy points ]" << std::endl;
-		std::cout << std::endl;
+		get_stats(bob);
 		for (int i = 0; i < 12; i++)
 		{
 			bob.attack("John");
